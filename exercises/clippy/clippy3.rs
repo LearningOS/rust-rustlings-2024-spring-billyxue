f71069b73f4bba72b567fa39025d0319e7c0892e
root <cracker0@126.com>
2024-04-20 17:02:47 +0800

    modify clippy3

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -1,17 +1,15 @@
 // clippy3.rs
 // 
 // Here's a couple more easy Clippy fixes, so you can see its utility.
-//
-// Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
-
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
+    /*
     if my_option.is_none() {
-        panic!();
-        // my_option.unwrap();
+        my_option.unwrap();
     }
+    */
 
     let my_arr = &[
         -1, -2, -3,
@@ -24,8 +22,8 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    // value_a = value_b;
-    // value_b = value_a;
+    //value_a = value_b;
+    //value_b = value_a;
     std::mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
